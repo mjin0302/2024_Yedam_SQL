1387097723a0f436de4b79521acfce7aafd676ef
mjin0302 <alswls7887@gmail.com>
2024-09-16 23:10:42 +0900

    to home

--- SQL_질의어/0912_Class.sql
+++ SQL_질의어/0912_Class.sql
@@ -1,6 +1,8 @@
 SELECT * 
   FROM employees;
 
+-- 내림차순, 오름차순 정렬 
+-- \order by coloum명 뒤에 적지 않으면 기본은 ASC정렬임
 SELECT   last_name,
          job_id,
          department_id,
@@ -28,7 +30,7 @@ FROM     employees
 ORDER BY department_id, salary DESC;
 
 -- 치환변수
--- &(싱글엔퍼센트) 한번 사용하고 메모리에서 사라짐
+-- 일시적변수 : &(싱글엔퍼센트) 한번 사용하고 메모리에서 사라짐
 SELECT employee_id,
        last_name,
        salary,
@@ -43,6 +45,7 @@ SELECT last_name,
        salary * 12
 FROM   employees
 WHERE  job_id = '&job_title';
+
 -- &&(더블 엔퍼센트) 메모리에서 직접 삭제하기 전까지는 메모리가 해당 값을 계속 가지고 있다. 
 SELECT   employee_id,
          last_name,
@@ -58,6 +61,7 @@ FROM   employees;
 UNDEFINE c_id;
 
 -- dual 테이블 유형? 조회
+-- DESC(describe)
 DESC dual;
 
 SELECT * 
@@ -66,6 +70,7 @@ FROM   dual;
 SELECT SYSDATE
 FROM   dual;
 
+-- 문자 함수 UPPER, LOWER
 SELECT 'The job id for ' || 
        UPPER(last_name) ||
        ' is ' ||
@@ -84,15 +89,15 @@ SELECT employee_id,
   FROM employees
  WHERE LOWER(last_name) = 'higgins';
  
+-- 문자 조작함수 
 -- TRIM ('d', FROM 'ddyedamd') -> yedam (앞, 뒤로 해당글자를 잘라낼 수 있음 단 한글자만 가능)
-
 SELECT  employee_id,
         CONCAT(first_name, last_name) NAME,
         job_id,
         LENGTH (last_name),
         INSTR(last_name, 'a') "Contains 'a'?"
   FROM  employees
- WHERE  SUBSTR(job_id, 4) = 'REP';
+ WHERE  SUBSTR(job_id, 4) = 'REP'; -- job_id의 4번째 부터 뒤까지의 구간이 REP 인 데이터
 
 select last_name,
        first_name
@@ -109,24 +114,37 @@ SELECT last_name,
 SELECT 20 || SUBSTR(hire_date, 1, 2)
 FROM    employees;
 
+-- 숫자 함수
+-- 반올림림
+SELECT  ROUND(45.923),    -- 45.923을 반올림 함 
+        ROUND(45.923 ,2), -- 45.923에서 반올림해서 소수점 두번째 자리 까지 나타냄  
+        ROUND(45.923 ,0), -- 45.923을 반올림 함 
+        ROUND(45.923 ,-1) -- 45.923에서 소수점 앞인 5를 반올림해서 결과값이 50이 나옴  
+FROM    DUAL;
+
+-- 버림 (반올림과 쓰는 형태는 동일 단지 함수만 다름) 
 SELECT TRUNC(45.923),
        TRUNC(45.923, 2), 
        TRUNC(45.923, 0),
        TRUNC(45.923, -1)
-  FROM DUAL;
+FROM   DUAL;
 
+-- 몫 구하는 함수 
 SELECT last_name,
        salary,
-       MOD(salary, 5000)
+       MOD(salary, 5000) -- salary를 5000으로 나눈 나머지
 FROM   employees;
 
+-- 날짜 함수  
 SELECT SYSDATE
 FROM   dual;
 
 SELECT TO_CHAR(SYSDATE, 'YYYY,MM,DD HH24:MI:SS')
 FROM   dual;
 
-SELECT last_name, (SYSDATE-hire_date) / 7 AS WEEKS
+-- 날짜 연산
+SELECT last_name, 
+       (SYSDATE - hire_date) / 7 AS WEEKS -- 주로 나타내는 법
   FROM employees
  WHERE department_id = '90';
  
@@ -134,12 +152,12 @@ SELECT employee_id,
        hire_date,
        MONTHS_BETWEEN(SYSDATE, hire_date) THNURE, -- 현재날짜 - 입사일 = 얼마나 근무 했는지
        ADD_MONTHS(hire_date, 6) REVIEW, -- 6개월 후 날짜
-       NEXT_DAY(hire_date, '금'), -- 입사일에 첫 번째 금요일은?
-       LAST_DAY(hire_date) -- 입사한 월에 마지막 날짜
+       NEXT_DAY(hire_date, '금'), -- 입사일에서 바로 다음 금요일
+       LAST_DAY(hire_date) -- 입사한 해당 월에 마지막 날
   FROM employees;
 
 SELECT NEXT_DAY(SYSDATE, '월'), -- 오늘 기준으로 돌아오는 월요일의 날짜를 출력
-       LAST_DAY(SYSDATE) -- 현재 월의 마지막 요일 출력
+       LAST_DAY(SYSDATE)       -- 현재 월의 마지막 요일 출력
   FROM dual;
 
 SELECT ROUND(SYSDATE, 'YEAR'),
@@ -148,11 +166,57 @@ SELECT ROUND(SYSDATE, 'YEAR'),
        ROUND(SYSDATE, 'DD')
 FROM   dual;
 
-SELECT TRUNC(SYSDATE, 'YEAR'), --
+SELECT TRUNC(SYSDATE, 'YEAR'),  -- 7월 1일 자정 기준
        TRUNC(SYSDATE, 'MONTH'), -- 15일 자정 기준
-       TRUNC(SYSDATE, 'DAY'), 
-       TRUNC(SYSDATE, 'DD')
+       TRUNC(SYSDATE, 'DAY'),   -- 수요일 자정 기준
+       TRUNC(SYSDATE, 'DD')     -- 오후 12시 기준
 FROM   dual;
+
+//---------변환함수----------------------// 
+SELECT *
+FROM   employees
+WHERE  department_id = '90';
+
+SELECT employee_id,
+       TO_CHAR(hire_date, 'MM/YY') AS Month_Hired
+FROM   employees;
+
+-- 날짜 세션 수정 - 아메리카~
+ALTER SESSION SET
+NLS_DATE_LANGUAGE = AMERICAN;
+
+SELECT last_name,
+       TO_CHAR(hire_date , 'DD Month YYYY')
+       AS HIREDATE
+FROM   employees;
+
+SELECT last_name,
+       TO_CHAR(hire_date , 'DD month YYYY')
+       AS HIREDATE
+FROM   employees;
+
+SELECT last_name,
+       TO_CHAR(hire_date , 'fmDD month YYYY')
+       AS HIREDATE
+FROM   employees;
+
+SELECT last_name,
+       TO_CHAR (hire_date,'Ddspth "of" Month YYYY HH:MI:SS:AM')AS HIREDATE
+FROM   employees;
+
+SELECT TO_CHAR(salary , '$99,999,00' ) SALARY
+FROM   employees
+WHERE  last_name = 'Ernst';
+
+SELECT TO_CHAR(salary , 'L99,999,00' ) SALARY
+FROM   employees
+WHERE  last_name = 'Ernst';
+
+SELECT TO_NUMBER ('$3,400','$99,999')
+FROM   DUAL;
+
+SELECT TO_DATE('2010년, 02월','YYYY"년", MM"월"')
+FROM   DUAL;
 
 SELECT last_name, 
        TO_CHAR(salary, '$99999') salary
@@ -171,11 +235,12 @@ SELECT last_name,
 FROM   employees
 WHERE  TO_CHAR(hire_date, 'YYYY') < '2006';
 
+-- NVL() 
 SELECT last_name,
        salary,
        NVL(commission_pct, 0),
        (salary * 12) + (salary * 12 * NVL(commission_pct, 0)) AS AN_SAL,
-       (salary * 12) + (salary * 12*commission_pct) AS AN_SAL2
+       (salary * 12) + (salary * 12 * commission_pct) AS AN_SAL2
 FROM   employees;
 
 -- NVL은 앞에 표현식 데이터 타입과 뒤에 표현식 데이터 타입이 같아야한다.
@@ -228,6 +293,7 @@ SELECT   last_name,
          END) "REVISED_SALARY"
   FROM   employees
 ORDER BY last_name;
+
 -- DECODE
 SELECT last_name,
        job_id,
