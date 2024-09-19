f1959a9cca44406dc2fb01ebe717130cb17f7c86
mjin0302 <alswls7887@gmail.com>
2024-09-19 17:12:31 +0900

    to yedam

--- SQL_질의어/DML/0919_DML_Class.sql
+++ SQL_질의어/DML/0919_DML_Class.sql
@@ -59,5 +59,66 @@ INSERT INTO sales_reps(id,
                                               commission_pct
                                        FROM   employees
                                        WHERE  job_id LIKE '%REP%';
+UPDATE employees
+SET    department_id = 50
+WHERE  employee_id = 113;
 
-SELECT * FROM sales_reps;
+SELECT *
+FROM   employees
+WHERE  employee_id = 113;
+
+-- employees데이터를 copy_emp테이블에 복사함
+INSERT INTO copy_emp
+  SELECT *
+  FROM   employees;
+  
+SELECT * FROM copy_emp ORDER BY employee_id;
+  
+UPDATE copy_emp
+SET    department_id = 110;
+  
+SELECT * FROM copy_emp;
+
+UPDATE employees
+SET    job_id = (SELECT job_id
+                 FROM   employees
+                 WHERE  employee_id = 205),
+       salary = (SELECT salary
+                 FROM   employees
+                 WHERE  employee_id = 205)
+WHERE  employee_id = 113;
+SELECT * FROM employees WHERE employee_id IN (113, 205);
+
+SELECT * 
+FROM   departments;
+
+DELETE FROM departments
+WHERE  department_name = 'Finance';
+
+DELETE FROM copy_emp;
+
+ROLLBACK;
+
+SELECT * 
+FROM   copy_emp;
+
+TRUNCATE TABLE copy_emp;
+
+commit;
+
+INSERT INTO copy_emp
+  SELECT *
+  FROM   employees;
+
+SELECT *
+FROM   copy_emp;
+
+commit;
+UPDATE copy_emp
+SET    salary = 9999
+WHERE  employee_id = 176;
+
+SELECT *
+FROM   copy_emp
+WHERE  employee_id = 176;
+rollback;
